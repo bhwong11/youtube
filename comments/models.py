275b6ae3206822ea6fbc817839e9c9d52e98a580
bhwong11 <user@email.com>
2021-08-16 19:03:27 -0400

    added comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models import Model, ForeignKey,CharField
+from django.db.models.deletion import CASCADE
+from video.models import Video
 
 # Create your models here.
+#related name -> video.comments.all
+class Comment(Model):
+    video = ForeignKey(Video,on_delete=CASCADE,related_name="comments")
+    content = CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.pk} - {self.video.title}"
--- video/models.py
+++ video/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models import Model, CharField,PositiveIntegerField,TextField,URLField, DateTimeField
 
 # Create your models here.
+class Video(Model):
+    title = CharField(max_length=150, unique=True)
+    #by default all false
+    views=PositiveIntegerField(default=1)
+    description = TextField(max_length=1000)
+    link = URLField(max_length=500)
+    published_date = DateTimeField(auto_now_add=True)
+    tags=CharField(max_length=300,default="creator,stuff")
+    #for new fields always create
+    #make migrations when you make new model or column
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['published_date']
+        #ordering = ['-published_date'] will reverse the order
